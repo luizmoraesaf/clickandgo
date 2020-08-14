7aaf00d8ae6d59d4d2dd0f1221432b25ecdb042f
Luiz Antonio De Moraes Filho <lmoraes@safeweb.com.br>
2020-08-14 15:49:57 -0300

    new branch: alinecentini v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,17 @@ export class AppComponent {
    */
   public goTo(application: ApplicationEnum, cellphone = ''): void {
     if (application === ApplicationEnum.WhatsApp) {
-      window.open(`https://api.whatsapp.com/send?phone=${cellphone}`);
+      // window.open(`https://api.whatsapp.com/send?phone=${cellphone}`);
     } else if (application === ApplicationEnum.Instagram) {
-      window.open('https://www.instagram.com/santamora.store');
+      window.open('https://www.instagram.com/alinicentininteriores');
     } else if (application === ApplicationEnum.eCommerce) {
-      window.open('https://santamora.vendizap.com/vitrine');
+      // window.open('https://santamora.vendizap.com/vitrine');
+    } else if (application === ApplicationEnum.Localization) {
+      window.open('https://goo.gl/maps/whLC3LdboFaDaH8W6');
+    } else if (application === ApplicationEnum.Email) {
+      window.open('mailto:centinialine@gmail.com');
+    } else if (application === ApplicationEnum.External){
+      window.open('https://docs.google.com/forms/d/e/1FAIpQLSeeMFh_ISBs2tsmbaM50n5xio0F6VTFVrgTQLChB1vBfBW-Dw/viewform');
     }
   }
 
--- src/app/application-enum.model.ts
+++ src/app/application-enum.model.ts
@@ -2,5 +2,8 @@ export enum ApplicationEnum {
     Instagram = 1,
     WhatsApp = 2,
     Facebook = 3,
-    eCommerce = 4
+    eCommerce = 4,
+    Localization = 5,
+    Email = 6,
+    External = 7
 }
